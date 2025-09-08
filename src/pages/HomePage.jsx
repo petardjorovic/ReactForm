96a0f392f56d6fcc8a979aaecb0a2987d378df4f
petardjorovic <perodjorovic@gmail.com>
2025-09-08 09:35:13 +0200

    home main

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage from DEV</div>;
+  return (
+    <div>
+      HomePage from DEV
+      <p>Ovo je promena sa main grane</p>
+    </div>
+  );
 }
 
 export default HomePage;
